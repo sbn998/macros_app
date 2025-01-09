3db9b9e649cf9a0376553c54f711617692a563ac
sbn998 <sergib1998@gmail.com>
2025-01-09 18:03:31 +0100

    locked app into protrait mode

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,19 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter/services.dart';
 
 import 'package:macros_app/screens/home_screen.dart';
 // import 'package:macros_app/themes/light_theme.dart';
 // import 'package:macros_app/themes/dark_theme.dart';
 
 void main() {
-  runApp(DevicePreview(
-      enabled: !kReleaseMode,
-      builder: (context) => const ProviderScope(child: MyApp())));
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((_) {
+    runApp(DevicePreview(
+        enabled: !kReleaseMode,
+        builder: (context) => const ProviderScope(child: MyApp())));
+  });
 }
 
 class MyApp extends StatelessWidget {

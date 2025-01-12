f90cb4bd039ced6a535052b775c2f01edea90f24
sbn998 <sergib1998@gmail.com>
2025-01-12 21:51:00 +0100

    removed magic variables by creating new constant keys and using the already existing ones

--- lib/constants/map_entries.dart
+++ lib/constants/map_entries.dart
@@ -1,10 +1,9 @@
-// Entries for the Macros map
+// Constant map entries.
+const String kIdKey = 'id';
 const String kCaloriesKey = 'calories';
 const String kProteinKey = 'protein';
 const String kCarbsKey = 'carbs';
 const String kFatsKey = 'fats';
-
-// Entries for the TextFormFields map
 const String kFoodNameKey = 'food_name';
 const String kServingNameKey = 'serving_name';
 const String kServingQuantityKey = 'serving_quantity';
---
+++ lib/constants/table_names.dart
@@ -0,0 +1,1 @@
+const String kSavedFoodTable = 'saved_food';

bbb25c3a646c99d4c339b382e2b564182528bcb5
sbn998 <sergib1998@gmail.com>
2025-01-14 17:37:04 +0100

    created new widget for cleaner code

--- lib/constants/enums.dart
+++ lib/constants/enums.dart
@@ -1,4 +1,5 @@
 enum Mode {
   normal,
   adding,
+  editing,
 }
--- lib/dialogs/food_screen/details_dialog.dart
+++ lib/dialogs/food_screen/details_dialog.dart
@@ -1,41 +1,23 @@
 import 'package:flutter/material.dart';
 
 import 'package:macros_app/models/food_model.dart';
-import 'package:macros_app/widgets/food_screen/food_details/editing_details_modal.dart';
-import 'package:macros_app/widgets/food_screen/food_details/normal_details_modal.dart';
+import 'package:macros_app/widgets/food_screen/food_details/saved_food_dialog_content.dart';
 
 //TODO: Add some graphics to show the macros visually.
 
 void showFoodDetailsDialog(BuildContext context, Food tappedFood) async {
-  bool isEditing = false;
-
-  bool? result = await showModalBottomSheet(
+  showModalBottomSheet(
     context: context,
     isScrollControlled: true,
     useSafeArea: true,
     builder: (context) {
-      return StatefulBuilder(
-        builder: (context, setState) {
-          return Padding(
-            padding: EdgeInsets.only(
-              top: 16,
-              bottom: MediaQuery.of(context).viewInsets.bottom,
-            ),
-            child: isEditing
-                ? EditingSavedFoodModal(tappedFood: tappedFood)
-                : SavedFoodDetailsModal(
-                    onPressEdit: () {
-                      setState(() => isEditing = true);
-                    },
-                    tappedFood: tappedFood),
-          );
-        },
+      return Padding(
+        padding: EdgeInsets.only(
+          top: 16,
+          bottom: MediaQuery.of(context).viewInsets.bottom,
+        ),
+        child: SavedFoodDialogContent(tappedFood: tappedFood),
       );
     },
   );
-
-  // Resets _isEditing if user closes the modal with a gesture.
-  if (result == null) {
-    isEditing = false;
-  }
 }

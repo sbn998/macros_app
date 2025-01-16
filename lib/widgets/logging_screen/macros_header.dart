e3ff1f1528b4e6e76ad9840b122a8ce0e8cf81ac
sbn998 <sergib1998@gmail.com>
2025-01-16 20:15:59 +0100

    fixed bug not showing the add goals button if there aren't any goals for the day

--- lib/widgets/logging_screen/macros_header.dart
+++ lib/widgets/logging_screen/macros_header.dart
@@ -6,6 +6,7 @@ import 'package:macros_app/providers/calculated_macros_provider.dart';
 import 'package:macros_app/providers/daily_macro_goals_provider.dart';
 import 'package:macros_app/providers/date_provider.dart';
 import 'package:macros_app/widgets/logging_screen/macros_eaten_overview.dart';
+import 'package:macros_app/widgets/logging_screen/no_macro_goal_widget.dart';
 
 class MacrosHeader extends ConsumerStatefulWidget {
   const MacrosHeader({super.key});
@@ -36,50 +37,56 @@ class _MacrosHeaderState extends ConsumerState<MacrosHeader> {
 
     return dailyMacros.when(
       data: (dailyMacros) {
-        return Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            SizedBox(
-              height: 195.0,
-              child: PageView(
-                controller: _pageController,
-                onPageChanged: (int page) {
-                  setState(() {
-                    _currentPage = page;
-                  });
-                },
-                children: [
-                  MacrosEatenOverview(
-                      isLeft: true,
-                      dailyMacros: dailyMacros,
-                      loggedMacros: calculatedMacros),
-                  MacrosEatenOverview(
-                      isLeft: false,
-                      dailyMacros: dailyMacros,
-                      loggedMacros: calculatedMacros),
-                ],
+        if (dailyMacros.isNotEmpty) {
+          return Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              SizedBox(
+                height: 195.0,
+                child: PageView(
+                  controller: _pageController,
+                  onPageChanged: (int page) {
+                    setState(() {
+                      _currentPage = page;
+                    });
+                  },
+                  children: [
+                    MacrosEatenOverview(
+                        isLeft: true,
+                        dailyMacros: dailyMacros,
+                        loggedMacros: calculatedMacros),
+                    MacrosEatenOverview(
+                        isLeft: false,
+                        dailyMacros: dailyMacros,
+                        loggedMacros: calculatedMacros),
+                  ],
+                ),
               ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: List.generate(2, (index) {
-                return AnimatedContainer(
-                  duration: const Duration(milliseconds: 200),
-                  margin: const EdgeInsets.symmetric(horizontal: 4.0),
-                  height: 5.0,
-                  width: 5.0,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: _currentPage == index
-                        ? selectedPageIndicatorColor
-                        : unselectedPageIndicatorColor,
-                  ),
-                );
-              }),
-            ),
-            const SizedBox(height: 15.0),
-          ],
-        );
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: List.generate(2, (index) {
+                  return AnimatedContainer(
+                    duration: const Duration(milliseconds: 200),
+                    margin: const EdgeInsets.symmetric(horizontal: 4.0),
+                    height: 5.0,
+                    width: 5.0,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: _currentPage == index
+                          ? selectedPageIndicatorColor
+                          : unselectedPageIndicatorColor,
+                    ),
+                  );
+                }),
+              ),
+              const SizedBox(height: 15.0),
+            ],
+          );
+        } else {
+          return EmptyDailyMacroGoal(
+            date: ref.read(selectedDateProvider.notifier).state,
+          );
+        }
       },
       loading: () {
         return const CircularProgressIndicator();
